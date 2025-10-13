1c5397265ae9712dca2657944b854ab9e36099f1
MohamedElhadidy0019 <mohamed.elbassiony00@eng-st.cu.edu.eg>
2025-10-13 17:35:30 +0200

    ignore

--- start_train.sh
+++ start_train.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python train_baseline.py --data_dir /scratch/s52melba/CerraData_Project_Phenorob/kaggle_temp/cerradata_4mm --gpu_ids 0 --batch_size 16 --num_epochs 100
+python train_baseline.py --data_dir /scratch/s52melba/CerraData_Project_Phenorob/kaggle_temp/cerradata_4mm --gpu_ids 1 --batch_size 256 --num_epochs 10[00
